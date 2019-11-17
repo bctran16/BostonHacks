b3791255cac1e8601b8b7c001f570bf0f152f0a1
Bang <bangwft@gmail.com>
2019-11-16 22:50:06 -0500

    working view controllers

---
+++ BostonHacks2019/Starter.swift
@@ -0,0 +1,30 @@
+//
+//  Starter.swift
+//  BostonHacks2019
+//
+//  Created by Bang Tran on 11/16/19.
+//  Copyright © 2019 Bang Tran. All rights reserved.
+//
+
+import UIKit
+
+class Starter: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
